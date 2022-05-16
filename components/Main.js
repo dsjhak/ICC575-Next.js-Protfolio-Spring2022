6caab0070a6fe1df4e8bb5515bc5e28205a3e440
羊大人早安 <yangshuwen@yangshuwendeMacBook-Air.local>
2022-05-16 16:19:30 -0400

    changed the Main component

--- components/Main.js
+++ components/Main.js
@@ -1,6 +1,6 @@
 import styles from './main.module.scss'
 
-const Main = () => {
+const Main = ({children}) => {
     return <mian className={StyleSheet.main}>{children}</mian>
 }
 export default Main
